fe0e1d3c1fb7d39ef42ef7547105075301200eb6
tingyusu1786 <tingyusu1786@gmail.com>
2023-01-09 12:13:48 +0800

    Complete with while loop & slice

--- week-2/assignment-1.js
+++ week-2/assignment-1.js
@@ -1,5 +1,13 @@
 function max(numbers) {
-// your code here
+  let maxNumArray = numbers;
+  while (maxNumArray.length > 1) {
+    if (maxNumArray[0] > maxNumArray[1]) {
+      maxNumArray.splice(1, 1);
+    } else if (maxNumArray[0] < maxNumArray[1]) {
+      maxNumArray.splice(0, 1);
+    }
+  }
+  return maxNumArray[0]
 }
 console.log(max([1, 2, 4, 5])); // expected output: 5
 console.log(max([5, 2, 7, 1, 6])); // expected output: 7
