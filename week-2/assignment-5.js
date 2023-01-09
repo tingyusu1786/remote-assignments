13db10cac8ed765687b6bbf27bd8e963141f598c
tingyusu1786 <tingyusu1786@gmail.com>
2023-01-09 18:31:40 +0800

    Complete solution

--- week-2/assignment-5.js
+++ week-2/assignment-5.js
@@ -1,11 +1,20 @@
 function twoSum(nums, target) {
-// your code here
+  for (let i = 0; i < nums.length -1; i++) {
+    for (let j = 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j]
+      }
+    }
+  }
 }
+
+console.log(twoSum([2, 7, 11, 15], 9));
+
 /*
-For example:
-twoSum([2, 7, 11, 15], 9);
-Should returns:
-[0, 1]
-Because:
-nums[0] + nums[1] is 9
+  For example:
+    twoSum([2, 7, 11, 15], 9);
+  Should returns:
+    [0, 1]
+  Because:
+    nums[0] + nums[1] is 9
 */
