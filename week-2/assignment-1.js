04a1190864a7bc1a6bf384c5ec400ef384644c00
tingyusu1786 <tingyusu1786@gmail.com>
2023-01-09 12:25:42 +0800

    Complete using for loop

--- week-2/assignment-1.js
+++ week-2/assignment-1.js
@@ -1,13 +1,11 @@
 function max(numbers) {
-  let maxNumArray = numbers;
-  while (maxNumArray.length > 1) {
-    if (maxNumArray[0] > maxNumArray[1]) {
-      maxNumArray.splice(1, 1);
-    } else if (maxNumArray[0] < maxNumArray[1]) {
-      maxNumArray.splice(0, 1);
+  let maxNum = numbers[0];
+  for (var i = 1; i < numbers.length; i++) {
+    if (numbers[i] > maxNum) {
+      maxNum = numbers[i];
     }
   }
-  return maxNumArray[0]
+  return maxNum
 }
 console.log(max([1, 2, 4, 5])); // expected output: 5
 console.log(max([5, 2, 7, 1, 6])); // expected output: 7
