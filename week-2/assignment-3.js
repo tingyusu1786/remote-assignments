0ce2e130ff3885a2448d0493e6a537668cfb2b50
tingyusu1786 <tingyusu1786@gmail.com>
2023-01-09 18:20:01 +0800

    Correct indention

--- week-2/assignment-3.js
+++ week-2/assignment-3.js
@@ -8,20 +8,20 @@ function calculate(data) {
 }
 
 const discountedPrice = calculate({
-discount: 0.1,
-products: [
-{
-name: "Product 1",
-price: 100
-},
-{
-name: "Product 2",
-price: 700
-},
-{
-name: "Product 3",
-price: 250
-}
-]
+  discount: 0.1,
+  products: [
+    {
+      name: "Product 1",
+      price: 100
+    },
+    {
+      name: "Product 2",
+      price: 700
+    },
+    {
+      name: "Product 3",
+      price: 250
+    }
+  ]
 });
 console.log(discountedPrice) // show the total price of all products after applying a discount
