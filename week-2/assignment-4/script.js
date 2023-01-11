d3f807d18eaa5bf6268a84392a9b1fac523938c5
tingyusu1786 <tingyusu1786@gmail.com>
2023-01-11 11:44:18 +0800

    Sort code

--- week-2/assignment-4/script.js
+++ week-2/assignment-4/script.js
@@ -18,15 +18,17 @@ Request 2: Click to Show/Close Menu
 --------------
 */
 
-const btnMore = document.getElementById('btn-more');
-const boxMore = document.getElementById('box-more');
 
-btnMore.addEventListener('click', () => {
-  boxMore.style.display = 'grid';
-})
 
 /*
 --------------
 Request 3: Click to Show More Content Boxes
 --------------
 */
+
+const btnMore = document.getElementById('btn-more');
+const boxMore = document.getElementById('box-more');
+
+btnMore.addEventListener('click', () => {
+  boxMore.style.display = 'grid';
+})
