9fa166d9459866500a0203c785042fc717cd6635
tingyusu1786 <tingyusu1786@gmail.com>
2023-01-11 11:30:35 +0800

    Complete request 2 using getElementById

--- week-2/assignment-4/script.js
+++ week-2/assignment-4/script.js
@@ -1,7 +1,32 @@
 alert("test");
 
-const message = document.querySelector(".welcome-msg");
+/*
+--------------
+Request 1: Click to Change Text
+--------------
+*/
+
+const message = document.querySelector(".welcome");
 
 message.addEventListener("click", () => {
-  message.innerHTML = "Have a good time!";
+  message.innerHTML = "<h1 class='welcome-msg'>Have a good time!</h1>";
 })
+
+/*
+--------------
+Request 2: Click to Show/Close Menu
+--------------
+*/
+
+const btnMore = document.getElementById('btn-more');
+const boxMore = document.getElementById('box-more');
+
+btnMore.addEventListener('click', () => {
+  boxMore.style.display = 'grid';
+})
+
+/*
+--------------
+Request 3: Click to Show More Content Boxes
+--------------
+*/
