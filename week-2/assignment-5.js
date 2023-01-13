b8923a4d44459dd6d22ee0cce1160c9eb7edec34
tingyusu1786 <tingyusu1786@gmail.com>
2023-01-13 21:45:58 +0800

    Modify the for loop of algorithm 1

--- week-2/assignment-5.js
+++ week-2/assignment-5.js
@@ -1,7 +1,7 @@
 // algorithm 1
 function twoSum1(nums, target) {
   for (let i = 0; i < nums.length -1; i++) {
-    for (let j = 1; j < nums.length; j++) {
+    for (let j = i + 1; j < nums.length; j++) {
       if (nums[i] + nums[j] === target) {
         return [i, j];
       }
