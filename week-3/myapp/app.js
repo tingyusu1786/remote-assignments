978191717f50684f02547320da690a5b5eb0e7c8
tingyusu1786 <tingyusu1786@gmail.com>
2023-01-19 05:01:38 +0800

    Start myapp

---
+++ week-3/myapp/app.js
@@ -0,0 +1,65 @@
+// -------------------------
+// assignment 1
+// -------------------------
+
+// import the "express" module
+const express = require("express");
+const router = express.Router();
+
+// use express to create a server 
+const app = express();
+
+// app listens for HTTP requests on port 3000
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Server!! 😇"); 
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}! 😇`);
+});
+
+// -------------------------
+// assignment 2
+// -------------------------
+
+app.get("/getData", (req, res) => {
+  let number = req.query.number;
+  if (number == undefined) {
+    res.send("Lack of Parameter"); 
+  } else if (!(number > 0)) {
+    res.send("Wrong Parameter");
+  } else {
+    let sum = num => {
+      return num === 0 ? 0 : num + sum( num-1 );
+    };
+    res.send(`${sum(+number)}`); 
+  }
+});
+
+// -------------------------
+// assignment 3
+// -------------------------
+
+app.use(express.static("sum"));
+
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
