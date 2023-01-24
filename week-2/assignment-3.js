a46f6e53a9636271a000f1e5b7088762fcc1f805
tingyusu1786 <tingyusu1786@gmail.com>
2023-01-25 03:10:30 +0800

    Optimize code using only .reduce()

--- week-2/assignment-3.js
+++ week-2/assignment-3.js
@@ -20,9 +20,18 @@
 
 // using reduce()
 // 先用.map()把各product的price抓出來，再用.reduce()加總
+// function calculate(data) {
+//   let totalPrice = data.products.map(i => i.price)
+//                                 .reduce((a, b) => a + b);
+//   return totalPrice * (1-data.discount);
+// }
+
+// using reduce() (without .map())
 function calculate(data) {
-  let totalPrice = data.products.map(i => i.price)
-                                .reduce((a, b) => a + b);
+  let totalPrice = data.products.reduce((totalPrice, product) => {
+    totalPrice += product.price;
+    return totalPrice;
+  }, 0);
   return totalPrice * (1-data.discount);
 }
 
@@ -45,3 +54,5 @@ const discountedPrice = calculate({
 });
 
 console.log(discountedPrice) // show the total price of all products after applying a discount
+
+
