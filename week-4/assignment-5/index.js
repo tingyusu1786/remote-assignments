eb0e195f926a2de589aba3d397a2b4a0ecd84ec0
tingyusu1786 <tingyusu1786@gmail.com>
2023-02-03 02:24:07 +0800

    Modify code to use only one state in App

--- week-4/assignment-5/index.js
+++ week-4/assignment-5/index.js
@@ -1,44 +1,66 @@
-let initValue = -1;
-
-const Counter = (props) => {
-  const [num, setNum] = React.useState(props.initNum);
-
-  React.useEffect(() => setNum(num => (num+1)), [props.toAddone]);
-
-  return (
-    <div className="counter">
-      <button onClick={() => setNum(num + 1)}>+ 1</button>
-      <span>{num}</span>
-    </div>
-  );
-}
+let initValue = 0;
 
 const App = () => {
-  const [counters, setCounters] = React.useState([initValue,initValue,initValue]);
-  const [addone, setAddone] = React.useState(0);
+  const [counters, setCounters] = React.useState([
+    initValue,
+    initValue,
+    initValue,
+  ]);
+
+  const Counter = (props) => {
+    return (
+      <div className="counter">
+        <button
+          onClick={() => {
+            setCounters([
+              ...counters.slice(0, props.index),
+              counters[props.index] + 1,
+              ...counters.slice(props.index + 1),
+            ]);
+          }}
+        >
+          + 1
+        </button>
+        <span>{counters[props.index]}</span>
+      </div>
+    );
+  };
 
   const AllAddOne = () => {
-    return(
-      <button className="all-addone" onClick={()=> setAddone(curAdd => curAdd + 1)}>All + 1</button>
+    return (
+      <button
+        className="all-addone"
+        onClick={() =>
+          setCounters((prevCounters) => prevCounters.map((i) => i + 1))
+        }
+      >
+        All + 1
+      </button>
     );
-  }
+  };
 
   const AddCounter = () => {
     return (
-      <button className="add-counter" onClick={() => setCounters([...counters, initValue])}>Add a Counter</button>
+      <button
+        className="add-counter"
+        onClick={() => setCounters([...counters, initValue])}
+      >
+        Add a Counter
+      </button>
     );
-  }
+  };
 
   return (
-    <div className="counters">
+    <div>
       <AllAddOne />
-      {counters.map((i, index) => <Counter key={"counter" + index} initNum={counters[index]} toAddone={addone}/>)}
+      {counters.map((i, index) => (
+        <Counter key={"counter-" + index} index={index} />
+      ))}
       <AddCounter />
     </div>
   );
-}
+};
 
-
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(<App />);
